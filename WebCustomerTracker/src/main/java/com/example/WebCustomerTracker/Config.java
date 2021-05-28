413dd563e7d445687f95cc1542638a69698c732d
Pranav Choudhary <2018184@iiitdmj.ac.in>
2021-05-28 20:13:32 +0530

    Config file created

--- WebCustomerTracker/src/main/java/com/example/WebCustomerTracker/Config.java
+++ WebCustomerTracker/src/main/java/com/example/WebCustomerTracker/Config.java
@@ -1,10 +1,74 @@
 package com.example.WebCustomerTracker;
 
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import org.hibernate.SessionFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import java.beans.PropertyVetoException;
+import java.util.Properties;
 
 @Configuration
 @EnableWebMvc
+@EnableTransactionManagement
+@ComponentScan({"com.example.WebCustomerTracker"})
 public class Config {
+
+    @Bean
+    public InternalResourceViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+
+        viewResolver.setPrefix("/WEB-INF/view/");
+        viewResolver.setSuffix(".jsp");
+
+        return viewResolver;
+    }
+
+
+    @Bean(destroyMethod = "close")
+    public ComboPooledDataSource dataSource() throws PropertyVetoException {
+        ComboPooledDataSource comboPooledDataSource = new ComboPooledDataSource();
+        comboPooledDataSource.setDriverClass("com.mysql.cj.jdbc.Driver");
+        comboPooledDataSource.setJdbcUrl("jdbc:mysql://localhost:3306/web_customer_tracker?useSSL=false&amp;serverTimezone=UTC");
+        comboPooledDataSource.setUser("root");
+        comboPooledDataSource.setPassword("123456");
+        comboPooledDataSource.setMinPoolSize(5);
+        comboPooledDataSource.setMaxPoolSize(20);
+        comboPooledDataSource.setMaxIdleTime(30000);
+        return comboPooledDataSource;
+    }
+
+
+    @Bean
+    public LocalSessionFactoryBean sessionFactoryBean() throws PropertyVetoException {
+        LocalSessionFactoryBean localSessionFactoryBean = new LocalSessionFactoryBean();
+        localSessionFactoryBean.setDataSource(dataSource());
+//        localSessionFactoryBean.setPackagesToScan(com.example.WebCustomerTracker);
+        Properties hibernateProperties = new Properties();
+        hibernateProperties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
+        hibernateProperties.setProperty("hibernate.show_sql", "true");
+        localSessionFactoryBean.setHibernateProperties(hibernateProperties);
+        return localSessionFactoryBean;
+
+    }
+
+
+
+    @Bean
+    public HibernateTransactionManager hibernateTransactionManager() throws PropertyVetoException {
+        HibernateTransactionManager hibernateTransactionManager = new HibernateTransactionManager();
+
+        hibernateTransactionManager.setSessionFactory((SessionFactory) sessionFactoryBean());
+        return hibernateTransactionManager;
+    }
+
+
+
 }
